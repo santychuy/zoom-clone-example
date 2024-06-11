5dc5ba408339a33cd3fa25ef1d08a4718efe4802
Santiago Carrasco <jsanticc@hotmail.com>
2024-06-11 13:56:25 -0700

    type: add type for base url

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    NEXT_PUBLIC_BASE_URL: string;
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
     CLERK_SECRET_KEY: string;
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
